65cc4cae3605f897e42288569f33a8dc1a33ad31
YukthiJeewantha <yukthijeewantha@gmail.com>
2025-06-18 23:50:03 +0530

    Update Footer.js

--- ruralhands/src/Components/Footer/Footer.js
+++ ruralhands/src/Components/Footer/Footer.js
@@ -123,7 +123,14 @@ const RuralHandsFooter = () => {
         </div>
       </div>
 
-      
+      {/* Bottom Copyright Section */}
+      <div className="border-t border-orange-800/30">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+          <div className="text-center">
+            <p className="text-gray-400 text-sm">
+              © 2025 Rural Hands. All rights reserved.
+            </p>
+          </div>
         </div>
       </div>
     </footer>
