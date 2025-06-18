7f8b6502a9f714505c0f1809ff3443960bb9fd4a
YukthiJeewantha <yukthijeewantha@gmail.com>
2025-06-18 23:31:46 +0530

    Update NavigationBar.js

--- ruralhands/src/Components/NavigationBar/NavigationBar.js
+++ ruralhands/src/Components/NavigationBar/NavigationBar.js
@@ -1,9 +1,23 @@
+import { ChevronDown, Search, ShoppingCart, User } from 'lucide-react';
+import { useState } from 'react';
+import logo from "../../Assests/logo.png";
 
-export default function 
-() {
+const Navbar = () => {
+  const [isAccountOpen, setIsAccountOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const categories = [
+    'Home',
+    'Products', 
+    'Locator',
+    'About Us',
+    'Contact Us',
+    'Blog',
+  ];
+
   return (
-    <div>
-        
-    </div>
-  )
-}
+   
+  );
+};
+
+export default Navbar;
