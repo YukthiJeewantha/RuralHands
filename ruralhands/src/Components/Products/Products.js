5ec9031e52a67313c55bccee21444f50a3043b0f
YukthiJeewantha <yukthijeewantha@gmail.com>
2025-06-22 22:48:03 +0530

    Update Products.js

--- ruralhands/src/Components/Products/Products.js
+++ ruralhands/src/Components/Products/Products.js
@@ -32,4 +32,63 @@ const Products = () => {
     setSelectedProduct(null);
   };
 
+  const categories = [
+    {
+      id: 1,
+      name: 'Pottery',
+      icon: '🏺',
+      bgColor: 'bg-orange-500',
+      textColor: 'text-white'
+    },
+    {
+      id: 2,
+      name: 'Textiles',
+      icon: '🧵',
+      bgColor: 'bg-orange-100',
+      textColor: 'text-orange-800'
+    },
+    {
+      id: 3,
+      name: 'Woodwork',
+      icon: '🪵',
+      bgColor: 'bg-orange-100',
+      textColor: 'text-orange-800'
+    },
+    {
+      id: 4,
+      name: 'Jewelry',
+      icon: '💎',
+      bgColor: 'bg-orange-100',
+      textColor: 'text-orange-800'
+    },
+    {
+      id: 5,
+      name: 'Statues',
+      icon: '🗿',
+      bgColor: 'bg-orange-100',
+      textColor: 'text-orange-800'
+    },
+    {
+      id: 6,
+      name: 'Baskets',
+      icon: '🧺',
+      bgColor: 'bg-orange-100',
+      textColor: 'text-orange-800'
+    },
+    {
+        id: 7,
+        name: 'Kitchen items',
+        icon: '🍽️',
+        bgColor: 'bg-orange-100',
+        textColor: 'text-orange-800'
+    },
+    {
+      id: 8,
+      name: 'All Crafts',
+      icon: '🎨',
+      bgColor: 'bg-orange-100',
+      textColor: 'text-orange-800'
+    }
+  ];
+
   
