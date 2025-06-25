4f883963d7baa537979c73e5b0eecc4cb1abc31f
YukthiJeewantha <yukthijeewantha@gmail.com>
2025-06-25 23:28:25 +0530

    Create productController.js

---
+++ ruralhands-backend/controller/productController.js
@@ -0,0 +1,52 @@
+const Product = require("../model/productModel");
+const cloudinary = require("../config/cloudinary");
+
+exports.createProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      artist,
+      price,
+      badges,
+      category,
+      description,
+      features,
+      stockCount,
+      sellerId,
+    } = req.body;
+
+    if (!sellerId) {
+      return res.status(400).json({ message: "Seller ID is required" });
+    }
+
+    let imageUrl = "";
+
+    // Upload image to Cloudinary if file exists
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "products", // optional folder in Cloudinary
+      });
+      imageUrl = result.secure_url;
+    }
+
+    const newProduct = new Product({
+      name,
+      artist,
+      price,
+      imageUrl,
+      badges: JSON.parse(badges || "[]"),
+      category,
+      description,
+      features: JSON.parse(features || "[]"),
+      stockCount,
+      seller: sellerId,
+    });
+
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    console.error("Create Product Error:", error.message);
+    res.status(500).json({ message: "Failed to create product." });
+  }
+};
+
