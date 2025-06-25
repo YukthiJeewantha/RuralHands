3c82be459e14735c7957df214dfe996665e115b8
YukthiJeewantha <yukthijeewantha@gmail.com>
2025-06-25 23:39:57 +0530

    Update server.js

--- ruralhands-backend/server.js
+++ ruralhands-backend/server.js
@@ -1,7 +1,13 @@
 // server.js
-
 require("dotenv").config();
 const express = require("express");
 const shopRouter = require("./router/shopRoutes");
 const cors = require("cors");
 const mongoose = require("mongoose");
+
+// Import route modules
+const Log = require("./router/loginRouter");
+const ProductRouter = require("./router/productRoutes");
+const orderRoutes = require("./router/orderRoutes");
+
+const app = express();
