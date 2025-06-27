118bef07f11d69b62ce027e631c2078f59a27b8f
YukthiJeewantha <yukthijeewantha@gmail.com>
2025-06-27 21:55:48 +0530

    Update server.js

--- ruralhands-backend/server.js
+++ ruralhands-backend/server.js
@@ -1,4 +1,5 @@
 // server.js
+
 require("dotenv").config();
 const express = require("express");
 const shopRouter = require("./router/shopRoutes");
@@ -27,3 +28,18 @@ app.use("/api/orders", orderRoutes);
 // MongoDB connection
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
